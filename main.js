c1c92e5bbc9586eb1daaf30832d9e8e70f04dcfe
amelenty <ada.melentyeva@gmail.com>
2018-03-25 01:53:50 +0200

    refactor main.js

--- main.js
+++ main.js
@@ -6,34 +6,42 @@ var roleRepairer = require('role.repairer');
 module.exports.loop = function () {
 
     if (Game.time % 10 == 0) {
-        for(var name in Memory.creeps) {
-            if(!Game.creeps[name]) {
-                Game.spawns['Carthage'].spawnCreep([WORK, CARRY, MOVE], name, {
-                    memory: {role: Memory.creeps[name].role}
-                });
-                console.log('Recreating creep:', name);
-            }
-        }
+        respawnDeadCreeps()
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        switch(creep.memory.role) {
-            case 'builder':
-                roleBuilder.run(creep);
-                break;
-            case 'harvester':
-                roleHarvester.run(creep);
-                break;
-            case 'repairer':
-                roleRepairer.run(creep);
-                break;
-            case 'upgrader':
-                roleUpgrader.run(creep);
-                break;
-            default:
-                console.log('Role unknown: ' + creep.memory.role + creep.name)
-                roleHarvester.run(creep);
-        }
-    }
+    processRoles()
+}
+
+var processRoles = function () {
+  for (var name in Game.creeps) {
+      var creep = Game.creeps[name];
+      switch(creep.memory.role) {
+          case 'builder':
+              roleBuilder.run(creep);
+              break;
+          case 'harvester':
+              roleHarvester.run(creep);
+              break;
+          case 'repairer':
+              roleRepairer.run(creep);
+              break;
+          case 'upgrader':
+              roleUpgrader.run(creep);
+              break;
+          default:
+              console.log('Role unknown: ' + creep.memory.role + creep.name)
+              roleHarvester.run(creep);
+      }
+  }
+}
+
+var respawnDeadCreeps () {
+  for(var name in Memory.creeps) {
+      if(!Game.creeps[name]) {
+          Game.spawns['Carthage'].spawnCreep([WORK, CARRY, MOVE], name, {
+              memory: {role: Memory.creeps[name].role}
+          });
+          console.log('Recreating creep:', name);
+      }
+  }
 }
