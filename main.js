1b29a6909354b4ebc487e1aeac3253f811de41ea
amelenty <ada.melentyeva@gmail.com>
2018-03-25 01:13:34 +0200

    Initial strategy

---
+++ main.js
@@ -0,0 +1,39 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
+
+module.exports.loop = function () {
+
+    if (Game.time % 10 == 0) {
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                Game.spawns['Carthage'].spawnCreep([WORK, CARRY, MOVE], name, {
+                    memory: {role: Memory.creeps[name].role}
+                });
+                console.log('Recreating creep:', name);
+            }
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        switch(creep.memory.role) {
+            case 'builder':
+                roleBuilder.run(creep);
+                break;
+            case 'harvester':
+                roleHarvester.run(creep);
+                break;
+            case 'repairer':
+                roleRepairer.run(creep);
+                break;
+            case 'upgrader':
+                roleUpgrader.run(creep);
+                break;
+            default:
+                console.log('Role unknown: ' + creep.memory.role + creep.name)
+                roleHarvester.run(creep);
+        }
+    }
+}
